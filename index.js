429e595ebee36df65bb4070ccb8f0d69b7fb20ff
vitalii-panama <vitalii@superco.io>
2024-04-09 19:50:32 +0300

    add dotenv

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Shopify = require('shopify-api-node');
-
+require('dotenv').config()
 const app = express();
 
 // Use body-parser middleware to parse incoming JSON data
